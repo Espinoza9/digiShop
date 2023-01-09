77045659d245571366519e5b0fd0ae3da8f2c2b1
Lennon Martins <lennonmcarlos@gmail.com>
2023-01-09 15:08:45 -0400

    subindo projeto

---
+++ src/main/java/br/com/digix/digishop/DigishopApplication.java
@@ -0,0 +1,13 @@
+package br.com.digix.digishop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigishopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigishopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/digix/digishop/DigishopApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.digix.digishop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigishopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
